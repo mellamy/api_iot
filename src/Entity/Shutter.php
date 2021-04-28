936daa6e627b5d8b74c5d0b9bf385bd7b00a2007
mel <46848553+mellamy@users.noreply.github.com>
2021-04-28 09:41:41 +0200

    Mise en place des entités

---
+++ src/Entity/Shutter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShutterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ShutterRepository::class)
+ */
+class Shutter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $state;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getState(): ?bool
+    {
+        return $this->state;
+    }
+
+    public function setState(bool $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Weather.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WeatherRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WeatherRepository::class)
+ */
+class Weather
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $temperature;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $luminosity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTemperature(): ?float
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(float $temperature): self
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getLuminosity(): ?float
+    {
+        return $this->luminosity;
+    }
+
+    public function setLuminosity(float $luminosity): self
+    {
+        $this->luminosity = $luminosity;
+
+        return $this;
+    }
+}
